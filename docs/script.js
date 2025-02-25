3d4802fe5cb49b2e1521507c58432718cc5cc10b
artkumik <artkumik@gmail.com>
2025-02-25 10:58:07 -0500

    resume and reformat

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+function downloadFile() {
+    const link = document.createElement("a");
+    link.href = "files/resume.pdf"; // Replace with the actual file URL
+    link.download = "ResumeAK.pdf"; // Name of the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
